1926fadec72347a29818c007391d35ad6f4e4a85
bainbrid <bainbrid@users.noreply.github.com>
2022-11-08 15:57:13 +0100

    triv useful scripts

---
+++ production/scripts/check_crab_status.sh
@@ -0,0 +1,27 @@
+#crab status -d TriggerLuminosity_2022Sep30/crab_BuToKJpsi_Toee
+#crab status -d TriggerLuminosity_2022Sep30/crab_BuToKPsi2S_Toee
+#crab status -d TriggerLuminosity_2022Sep30/crab_BuToKee
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part0 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part1 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part2 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part3 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part4 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022C_part5 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part0 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part1 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part2 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part3 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part4 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv1_part5 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part0 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part1 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part2 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part3 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part4 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+#crab status -d TriggerLuminosity_2022Oct02/crab_Run2022Dv2_part5 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part0 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part1 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part2 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part3 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part4 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
+crab status -d TriggerLuminosity_2022Nov07/crab_Run2022E_part5 | grep -e "Task name" -e finished -e transferring -e running -e "failed  " -e idle
---
+++ production/scripts/check_datasets_in_das.sh
@@ -0,0 +1,1 @@
+dasgoclient --query="dataset=/ParkingDoubleElectronLowMass*/Run2022*-PromptReco-v*/MINIAOD" >& datasets.txt
