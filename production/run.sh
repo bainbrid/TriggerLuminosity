dca0900a633a269d0814e63ca901f911d0cbc8a1
bainbrid <bainbrid@users.noreply.github.com>
2022-11-08 14:56:49 +0100

    add all eras and MC (all commented)

--- production/run.sh
+++ production/run.sh
@@ -1,1 +1,9 @@
-python3 submit_on_crab.py # --filter=Run2022Dv1_part0
+# Uncommented as required ...
+#python3 submit_on_crab.py --filter=Run2022C_part*
+#python3 submit_on_crab.py --filter=Run2022Dv1_part*
+#python3 submit_on_crab.py --filter=Run2022Dv2_part*
+#python3 submit_on_crab.py --filter=Run2022E_part*
+#python3 submit_on_crab.py --filter=Run2022F_part*
+#python3 submit_on_crab.py --filter=BuToKee
+#python3 submit_on_crab.py --filter=BuToKJpsi_Toee
+#python3 submit_on_crab.py --filter=BuToKPsi2S_Toee
